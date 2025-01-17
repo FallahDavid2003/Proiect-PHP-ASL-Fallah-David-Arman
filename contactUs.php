f56eab1aa583f7a8e839cbf986af697879e56526
FallahDavid2003 <aslfallah.e.davidarman22@stud.rau.ro>
2025-01-17 16:28:53 +0200

    Add files via upload

---
+++ contactUs.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OG Tech - Contact US</title>
+  <?php include "header.php"; ?>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+  <h1 class="page-title grid background-overlay" style="margin:1px;padding-top: 140px;padding-bottom: 140px;">Contact Us</h1>
+    <div class="grid" style="margin:0px 15% 0px 15%">
+        <div id="map">
+            <iframe src="https://www.google.com/maps/dir//Tehran+District+6+Valiasr+Street+Above+Taleghani+Crossroads,+Iran/@35.7080982,51.3235578,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3f8e0113c0b0d53f:0x496e49092748f80d!2m2!1d51.4059589!2d35.7081263?entry=ttu&g_ep=EgoyMDI1MDExNC4wIKXMDSoASAFQAw%3D%3D" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div id="contactdetailscontainer">
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">place</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:22px">OG Solutions (registered name)</strong><br>
+                    
+Address
+
+Sabalan Office: No. 92, Mollasadra Ave., Vanak Sq., Tehran, Iran
+
+
+Sabalan Factory: No 23-24, Enghelab St.,Parsa Sq., Aboul Ghassem Blvd Ahmadabad Mostofi, Tehran, Iran
+
+
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">phone</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:22px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:23px">Contact Number</strong><br>
+                    Tel +98 88030691-2
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">email</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">OG Solutions</strong><br>
+                    ogtech@gmail.com
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">alarm</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">Business Hours</strong><br>
+                    Monday to Sunday: 11.00am to 8.00pm
+                </p>
+            </div>
+        </div>
+    </div>  
+
+
+  <?php require "footer.php"; ?>
+</body>
+</html>
